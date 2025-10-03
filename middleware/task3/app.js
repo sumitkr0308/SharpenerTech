0260dea4d1ed3867e4d7378ce673428b7846de28
sumitkr0308 <sumit2k30@gmail.com>
2025-10-03 13:33:39 +0530

    unknown route is solved

---
+++ middleware/task3/app.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const app=express();
+const PORT=4000;
+
+app.use(express.json());
+
+app.get("/products",(req,res)=>{
+    res.send("Here is the list of all products.");
+})
+
+app.post("/products",(req,res)=>{
+    res.send("A new product has been added.");
+})
+
+app.get("/categories",(req,res)=>{
+    res.send("Here is the list of all categories.");
+})
+
+app.post("/categories",(req,res)=>{
+    res.send("A new category has been created.");
+})
+
+// unknown routes 
+app.use((req,res)=>{
+    res.status(404).send(`<h1>404 - Page Not Found</h1>`);
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
