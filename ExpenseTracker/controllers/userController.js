ffccbd78fb2cd2d03b2e3319f0b7c301a36c366a
sumitkr0308 <sumit2k30@gmail.com>
2025-11-11 14:30:55 +0530

    secure password

--- ExpenseTracker/controllers/userController.js
+++ ExpenseTracker/controllers/userController.js
@@ -1,5 +1,6 @@
 const user=require("../models/signupUser");
 const path=require("path");
+const bcrypt=require("bcrypt");
 
 const getSignPage=(req,res)=>{
 
@@ -11,12 +12,26 @@ const getSignPage=(req,res)=>{
 const signupUser=async(req,res)=>{
     try {
         const {name,email,password}=req.body;
-        const signupUsers= await user.create({
-            name,
-            email,
-            password
-        })
-        res.status(201).json(signupUsers);
+        // validate
+        if(!name || !email || !password)
+        {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        // check already existed user
+        const existingUser = await user.findOne({ where: { email } });
+            if (existingUser) {
+            return res.status(409).json({ message: "Email already registered" });
+        }
+
+        // hashed password
+        const hashedPassword=await bcrypt.hash(password,10);
+       
+        // create user
+      const signupUsers= await user.create({name,email,password:hashedPassword});
+
+     res.status(201).json({message: "Signup successful",user: signupUsers,});
+          
     } catch (error) {
         console.log(error)
         res.status(500).json({error:error.message});
@@ -27,17 +42,21 @@ const signupUser=async(req,res)=>{
 const loginUser=async(req,res)=>{
     try {
         const {email,password}=req.body;
+        // validate
         if (!email || !password) {
          return res.status(400).json({ message: "All fields are required." });
         }
+        // check user
       const existingUser = await user.findOne({ where: { email } });
         
         if (!existingUser) {
          return res.status(404).json({ message: "User not found." });
         }
         
+        // check valid password
+        const validPassword=await bcrypt.compare(password,existingUser.password);
         // password not match
-        if (existingUser.password !== password) {
+        if (!validPassword) {
          return res.status(401).json({ message: "Invalid credentials." });
          }  
         
